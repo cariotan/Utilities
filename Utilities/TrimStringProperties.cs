3d54fbc72486e4dfbc6aa80fe0258779bc2604fe
Cario Tan <a@cario.me>
2023-06-28 11:37:47 +1000

    initial commit

---
+++ Utilities/TrimStringProperties.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+
+namespace Utilities;
+
+public static partial class ExtensionMethods
+{
+	public static void TrimStringProperties(this object obj)
+	{
+		PropertyInfo[] properties = obj.GetType().GetProperties();
+
+		foreach(PropertyInfo property in properties)
+		{
+			if(property.PropertyType != typeof(string))
+			{
+				continue;
+			}
+
+			string value = (string)property.GetValue(obj);
+
+			if(value == null)
+			{
+				continue;
+			}
+
+			value = value.Trim();
+			property.SetValue(obj, value);
+		}
+	}
+}
